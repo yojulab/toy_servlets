ffb88da24397612107493dd575a56bfb3f361828
yojulab <otter.oh@gmail.com>
2022-12-15 09:29:56 +0900

    first commit

---
+++ src/main/java/com/yojulab/toy_servlets/ToyServletsApplication.java
@@ -0,0 +1,13 @@
+package com.yojulab.toy_servlets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyServletsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyServletsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yojulab/toy_servlets/ToyServletsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yojulab.toy_servlets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyServletsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
